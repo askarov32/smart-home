4ef4b147a2c05dcf2f286c4e92fb503517602a7b
askarov32 <insar@lessons.kz>
2024-07-01 23:48:07 +0500

    Initial commit

---
+++ src/main/java/main/google/smart_home/SmartHomeApplication.java
@@ -0,0 +1,13 @@
+package main.google.smart_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeApplication.class, args);
+	}
+
+}
